dacb853628a0515598e529d1a4fd6759f09ede0d
AlexandruMunteanuIoan <munteanu.sas@gmail.com>
2023-02-26 18:08:12 +0200

    Contact page, contact style and corect the bugs

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 const main = document.querySelector('.main');
+const contact = document.querySelector('.contact');
 const boxAL = document.querySelector('.boxAL');
 const loginLink = document.querySelector('.loginLink');
 const registerLink = document.querySelector('.registerLink');
 const btnLogin = document.querySelector('.btnLogin');
 const closeIcon = document.querySelector('.closeIcon');
+const btnContact = document.querySelector('.btnContact');
 
-registerLink.addEventListener('click', ()=>
-{
+registerLink.addEventListener('click', () => {
     boxAL.classList.add('active');
 });
 
-loginLink.addEventListener('click', ()=>
-{
+loginLink.addEventListener('click', () => {
     boxAL.classList.remove('active');
 });
 
-btnLogin.addEventListener('click', ()=>
-{
+btnLogin.addEventListener('click', () => {
     boxAL.classList.add('active-btnLogin');
 });
 
-btnLogin.addEventListener('click', ()=>
-{
+btnLogin.addEventListener('click', () => {
     boxAL.classList.add('active-btnLogin');
     main.classList.add('active-btnLogin');
+    contact.classList.add('active-btnLogin');
 });
 
-closeIcon.addEventListener('click', ()=>
-{
+closeIcon.addEventListener('click', () => {
     boxAL.classList.remove('active-btnLogin');
     main.classList.remove('active-btnLogin');
+    contact.classList.remove('active-btnLogin');
 });
+
+btnContact.addEventListener('click', showMessage());
+function showMessage() {
+    document.getElementById("sendMessage").style.visibility = "visible"
+};
+
+function hiddenMessage() {
+    document.getElementById("sendMessage").style.visibility = "hidden"
+} setTimeout("hiddenMessage()", 5000);
