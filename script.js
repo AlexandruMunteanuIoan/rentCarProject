7714407818fc6559ed06e6262300054866b6ab81
AlexandruMunteanuIoan <munteanu.sas@gmail.com>
2023-02-26 12:08:57 +0200

    Add little changes in all files

---
+++ script.js
@@ -0,0 +1,30 @@
+const boxAL = document.querySelector('.boxAL');
+const loginLink = document.querySelector('.loginLink');
+const registerLink = document.querySelector('.registerLink');
+const btnLogin = document.querySelector('.btnLogin');
+const closeIcon = document.querySelector('.closeIcon');
+
+registerLink.addEventListener('click', ()=>
+{
+    boxAL.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=>
+{
+    boxAL.classList.remove('active');
+});
+
+btnLogin.addEventListener('click', ()=>
+{
+    boxAL.classList.add('active-btnLogin');
+});
+
+btnLogin.addEventListener('click', ()=>
+{
+    boxAL.classList.add('active-btnLogin');
+});
+
+closeIcon.addEventListener('click', ()=>
+{
+    boxAL.classList.remove('active-btnLogin');
+});
